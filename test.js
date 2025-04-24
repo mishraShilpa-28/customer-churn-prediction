491f15ec4a70177be34b6d8b0256c6a6a7bec7b1
Shilpa <Shilpamishramiss@gmail.com>
2025-04-24 14:03:57 +0530

    Create test.js

---
+++ test.js
@@ -0,0 +1,4 @@
+console.log(“ram” && 56 && 0n && true)
+console.log(“ram” && 56 && true)
+console.log(false || ”” || null ||  {} || undefined)
+console.log(false || ”” || null ||  {} || 0 || undefined)
